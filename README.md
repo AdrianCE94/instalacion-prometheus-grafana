1d904496a14f0af5f82203c81798bb4e23a176c5
AdrianCE94 <adriance4jobby@gmail.com>
2024-12-02 18:44:23 +0100

    Add installation guide and automation script for Prometheus and Grafana

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # instalacion-prometheus-grafana
 Tutorial para instalar prometheus y grafana
+
+# CONCEPTOS
+PROMETHEUS → HERRAMIENTA DE MONITORIZACION
+NODE EXPORT → LIBRERIA PARA LAS METRICAS
+GRAFANA → PARA VERLO GRAFICAMENTE
+
+![graf](image.png)
+
+# Instalación de Prometheus y Grafana
+Tengo una máquina Debian12 a la cual nos vamos a conectar pos ssh para que sea mas comodo. Me paso a root
+
+## Prometheus
+```bash
+# Actualizamos el sistema
+apt install update
+# Instalamos prometheus
+apt install prometheus -y
+systemctl daemon-reload
+# Comprobamos que el servicio esta corriendo
+systemctl status prometheus
+```
+## Grafana
+```bash
+# Instalacion de paquetes necesarios
+apt install -y apt-transport-https
+software-properties-common wget
+# Añadimos la clave de grafana
+wget -q -O - https://packages.grafana.com/gpg.key | apt-key add -
+# Añadimos el repositorio de grafana
+echo "deb https://packages.grafana.com/oss/deb stable main" | tee -a /etc/apt/sources.list.d/grafana.list
+# si queremos buscar la key
+apt-key list | grep grafana
+# Actualizamos los repositorios
+apt update
+# Instalamos grafana
+apt install grafana -y
+# Encendemos el servicio
+systemctl daemon-reload
+systemctl enable grafana-server
+systemctl start grafana-server
+```
+
+# ACCESO
+para acceder a grafana, abrimos un navegador y ponemos la ip de la maquina y el puerto 3000. Por defecto el usuario y contraseña es admin/admin y nos pedira cambiar la contraseña.
+
+# Script de instalación
+Si quieres automatizar la instalación, puedes usar el script de instalación que esta en el repositorio.
+```bash	
+git clone https://github.com/AdrianCE94/instalacion-prometheus-grafana
+cd instalacion-prometheus-grafana
+chmod a+x instalaPromGraf.sh
+./instalaPromGraf.sh
+```
---
+++ instalaPromGraf.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# Script para instalar Prometheus y Grafana en un sistema Linux
+# Autor: Adrian Cabezuelo Exposito
+# Fecha: 10/11/2020
+# gh --> https://github.com/AdrianCE94
+
+echo "Bienvenido al asistente de instalación automática de Prometheus y Grafana en el sistema"
+
+actualizarSistema() {
+    echo "Actualizando sistema...."
+    apt update
+}
+
+instalarPrometheus() {
+    echo "Instalando Prometheus...."
+    apt install -y prometheus
+}
+
+instalarGrafana() {
+    echo "Instalando Grafana...."
+    echo "Instalando paquetes necesarios"
+    apt install -y apt-transport-https software-properties-common wget
+
+    echo "Añadiendo clave de Grafana"
+    wget -q -O - https://packages.grafana.com/gpg.key | apt-key add -
+
+    echo "Añadiendo repositorio de Grafana"
+    echo "deb https://packages.grafana.com/oss/deb stable main" | tee /etc/apt/sources.list.d/grafana.list
+
+    echo "Actualizando repositorios"
+    apt update
+
+    echo "Instalando Grafana"
+    apt install -y grafana
+
+    echo "Encendiendo el servicio de Grafana"
+    systemctl daemon-reload
+    systemctl enable grafana-server
+    systemctl start grafana-server
+
+    echo "Grafana instalado correctamente"
+    echo "Puede acceder a Grafana en http://localhost:3000"
+}
+
+
+menu(){
+    clear
+    echo "1. Instalar Prometheus"
+    echo "2. Instalar Grafana"
+    echo "3. Salir"
+}
+
+menu
+read -p "Seleccione una opción: " opcion
+while [ "$opcion" != 3 ]; do
+    case $opcion in
+        1)
+            actualizarSistema
+            instalarPrometheus
+            ;;
+        2)
+            instalarGrafana
+            ;;
+        3)
+            echo "Saliendo del asistente de instalación,Gracias por usarlo"
+            ;;
+        *)
+            echo "Opción incorrecta"
+            ;;
+    esac
+    read -p "Pulse una tecla para continuar..." intro
+    menu
+    read -p "Seleccione una opción: " opcion
+done
